9aec15087923146873e68e5fabea4f19ff7742eb
ajemraou <ajemraou@student@1337.ma>
2025-06-28 14:57:57 +0100

    remove the label

--- mysite/meetups/forms.py
+++ mysite/meetups/forms.py
@@ -3,7 +3,7 @@ from .models import Participant
 
 class RegistrationForm(forms.Form):
   email = forms.EmailField(
-      label='Your Email',
+    label='',
       max_length=254,
       widget=forms.EmailInput(attrs={
           'placeholder': 'Enter your email',
