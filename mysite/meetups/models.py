87090abae2c73189f5091ee573242a10a94be73d
ajemraou <ajemraou@student@1337.ma>
2025-06-28 12:21:56 +0100

    create core meetup models with relationships

--- mysite/meetups/models.py
+++ mysite/meetups/models.py
@@ -5,9 +5,6 @@ from django.db import models
 class Location(models.Model):
   name = models.CharField(max_length=100)
   address = models.CharField(max_length=200)
-  # city = models.CharField(max_length=100)
-  # state = models.CharField(max_length=100)
-  # zip_code = models.CharField(max_length=20)
 
   def __str__(self):
     return f'{self.name}, ({self.address})'
