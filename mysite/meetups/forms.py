ef569e7207b7d47879ef382a949dcb8eaa55c49f
ajemraou <ajemraou@student@1337.ma>
2025-06-28 12:20:57 +0100

    implement user registration form

--- mysite/meetups/forms.py
+++ mysite/meetups/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import Participant
 
-class RegistrationForm(forms.ModelForm):
-  class Meta:
-    model = Participant
-    fields = ['email']
+class RegistrationForm(forms.Form):
+  email = forms.EmailField(
+      label='Your Email',
+      max_length=254,
+      widget=forms.EmailInput(attrs={
+          'placeholder': 'Enter your email',
+          'class': 'form-control',
+      })
+  )
